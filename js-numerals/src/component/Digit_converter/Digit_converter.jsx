abbbafd7d5d76d58027b9ae3d166d01cc31d0cda
turangabor <>
2020-10-07 20:04:23 +0200

    add material, css prettier and  base html

--- js-numerals/src/component/Digit_converter/Digit_converter.jsx
+++ js-numerals/src/component/Digit_converter/Digit_converter.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import "./Digit_converter.css";
 
 function Digit_converter() {
-  return <div></div>;
+  return (
+    <div className="digit_converter">
+      <h1>Digit converter</h1>
+      <form id="digit_form">
+        <TextField
+          id="digit_input"
+          label="Digit"
+          type="number"
+          InputProps={{
+            inputProps: {
+              min: 0
+            }
+          }}
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+        <Button variant="contained" color="secondary">
+          Convert
+        </Button>
+      </form>
+      <div className="digit_converter_result">
+        <h3>Result</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Digit_converter;
